2b6a19231fd4b98a39fd93d8d713c111218b3ebc
aadil <aadil6134@gmail.com>
2024-06-08 19:18:15 +0530

    test for logs

--- database.js
+++ database.js
@@ -1,11 +1,22 @@
-const mysql = require('mysql');
+// const mysql = require('mysql');
 
-const conn = mysql.createConnection({
-  host: "SG-myApps-8768-mysql-master.servers.mongodirector.com",
-  user: "bloguser",
-  password: "Bloguser@123",
-  database: "blogdata",
-  port: 3306,
+// const conn = mysql.createConnection({
+//   host: "SG-myApps-8768-mysql-master.servers.mongodirector.com",
+//   user: "bloguser",
+//   password: "Bloguser@123",
+//   database: "blogdata",
+//   port: 3306,
+// });
+
+// module.exports = conn;
+
+const mysql = require("serverless-mysql")({
+  config: {
+    host: "SG-myApps-8768-mysql-master.servers.mongodirector.com",
+    database: "blogdata",
+    user: "bloguser",
+    password: "Bloguser@123",
+    port: 3306,
+  },
 });
-
-module.exports = conn;
+module.exports = mysql;
--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const connection = require("./database");
+const mysql = require("./database");
 const app = express();
 
 const PORT = process.env.PORT || 5000;
@@ -7,13 +7,13 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
-connection.connect((err) => {
-  if (err) {
-    console.log("DB connection Error");
-  } else {
-    console.log("DB connected successfully");
-  }
-});
+// connection.connect((err) => {
+//   if (err) {
+//     console.log("DB connection Error from code", err);
+//   } else {
+//     console.log("DB connected successfully");
+//   }
+// });
 
 // var sql = "CREATE TABLE blogs (title VARCHAR(255), content VARCHAR(255))";
 // connection.query(sql, function (err, result) {
@@ -22,16 +22,16 @@ connection.connect((err) => {
 // });
 
 app.get("/", (req, res) => {
-  res.send("Express on Vercel");
+  res.send("Express on Vercel - updated new");
 });
 
-app.get("/blogs", (req, res) => {
+app.get("/blogs", async (req, res) => {
   var sql = "SELECT * FROM blogs";
-
-  connection.query(sql, function (err, result) {
-    if (err) throw err;
-    res.status(200).json(result);
-  });
+  console.log("Started executing the query");
+  const resp = await mysql.query(sql);
+  console.log("Ended executing the query");
+  mysql.end();
+  res.status(200).send(resp);
 });
 
 module.exports = app;
